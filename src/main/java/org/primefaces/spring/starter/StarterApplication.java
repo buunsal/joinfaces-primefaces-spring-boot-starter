bebecac088288edd98e0a34e4231e4b705366003
burak <m.buunsal@gmail.com>
2021-04-02 22:54:38 +0300

    refactor

--- src/main/java/org/primefaces/spring/starter/StarterApplication.java
+++ src/main/java/org/primefaces/spring/starter/StarterApplication.java
@@ -2,18 +2,12 @@ package org.primefaces.spring.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class StarterApplication extends ServletInitializer {
+public class StarterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StarterApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(StarterApplication.class);
     }
 
 }
